1323a068006fb6802b86da2115ccabb7868634f8
Dincho98 <malcom.houston98@gmail.com>
2019-08-19 09:28:00 +0200

    Initial commit

---
+++ PC CAFFE/bin/Debug/DBbackup/DBbackup.bat
@@ -0,0 +1,1 @@
+pg_dump.exe --host localhost --port 5432 --username postgres --format custom --blobs --verbose --file "C:\Users\Korisnik\Desktop\Sigurnosna kopija baze 2019\db2019-08-19.backup" "db2019"
